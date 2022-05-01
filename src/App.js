c05a7242bf2c635a81130f3a4b384d09c83fcf7e
Lap Van Pham <caramel-apple@outlook.com>
2022-05-01 10:04:38 +0700

    f commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      item: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ item: e.target.value });
+  };
+
+  handleAddItem = () => {
+    const newList = [...this.state.list, this.state.item];
+    this.setState({ list: newList, item: '' });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className='App'>
+        <h1>Todo List</h1>
+        <input type='text' onChange={this.handleChange} />
+        <button onClick={this.handleAddItem}>Add</button>
+        <ul className='items-container'>
+          {this.state.list.map((item) => {
+            return (
+              <li key={item} className='todo'>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
